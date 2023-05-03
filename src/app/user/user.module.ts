cc88f589ebe700d51e07a3ddadfcff711623c580
OleksaMarko <kompotius43@gmail.com>
2023-05-03 22:50:03 +0300

    linting

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,13 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserComponent } from './user.component';
-import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
-import { MatSelectModule } from '@angular/material/select';
 import { UserService } from './user.service';
-import { MatCheckboxModule } from '@angular/material/checkbox';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
@@ -16,12 +13,9 @@ import { MatSnackBarModule } from '@angular/material/snack-bar';
   declarations: [UserComponent],
   imports: [
     CommonModule,
-    MatToolbarModule,
     MatInputModule,
     MatButtonModule,
     MatCardModule,
-    MatSelectModule,
-    MatCheckboxModule,
     ReactiveFormsModule,
     HttpClientModule,
     MatSnackBarModule,
